ef6ed953c0679d9eb6ccf373fd3f5bea1d55f0a8
alexiliev <iliev.alex@outlook.com>
2019-10-07 14:11:15 -0700

    creating menu-item component

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -4,12 +4,7 @@ import './homepage.styles.scss'
 const HomePage = () =>  (
     <div className='homepage'>
         <div className='directory-menu'>
-            <div className='menu-item'>
-                <div className='content'>
-                    <h1 className='title'>Pucks</h1>
-                    <span className='subtitle'>Shop Now</span>
-                </div>
-            </div>
+            
             <div className='menu-item'>
                 <div className='content'>
                     <h1 className='title'>Sticks</h1>
