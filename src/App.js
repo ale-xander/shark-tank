ab6f36b5398c5120e7dd44b276ee988dee898e13
alexiliev <iliev.alex@outlook.com>
2019-10-07 18:04:46 -0700

    adding basic routing for one component, pucks

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
 import './App.css';
 import HomePage from './pages/homepage/homepage.component'
+
+const PucksPage = () => (
+  <div>
+    <h1>Pucks page </h1>
+  </div>
+);
 
 function App() {
   return (
     <div className="App">
-     <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop/pucks' component={PucksPage} />
+      </Switch>
     </div>
   );
 }
