dc748300201ad958b4184f53953222ecb54b9384
alexiliev <iliev.alex@outlook.com>
2019-10-08 23:14:58 -0700

    implemented Google auth

--- src/App.js
+++ src/App.js
@@ -10,6 +10,24 @@ import {auth} from './firebase/firebase.utils'
 
 
 class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      currentUser: null
+    }
+  };
+
+  unsubscribeFromAuth = null;
+  componentDidMount(){
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({currentUser: user});
+    })
+  };
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+  
   render(){
     return (
       <div className="App">
