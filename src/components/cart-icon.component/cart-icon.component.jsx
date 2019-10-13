69d5052aa2ed46e6178f0d7d225e2f03b982e5f0
alexiliev <iliev.alex@outlook.com>
2019-10-13 15:27:01 -0700

    implementing cart reducer. cart dropdown now fixed

--- src/components/cart-icon.component/cart-icon.component.jsx
+++ src/components/cart-icon.component/cart-icon.component.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
-import {ReactComponent as ShoppingIcon} from '../../assets/iconmonstr-shopping-bag-4.svg';
+import { connect } from 'react-redux';
+import { ToggleCartHidden } from '../../redux/cart/cart.actions';
+import { ReactComponent as ShoppingIcon } from '../../assets/iconmonstr-shopping-bag-4.svg';
 import './cart-icon.styles.scss';
 
-const CartIcon = () => (
-    <div className="cart-icon">
+const CartIcon = ({ToggleCartHidden}) => (
+    <div className="cart-icon" onClick={ToggleCartHidden}>
         <ShoppingIcon className='shopping-icon'/>
         <span className='item-count'>0</span>
     </div>
-)
-export default CartIcon;
+);
+
+const mapDispatchToProps = dispatch => ({
+    ToggleCartHidden: ()=> dispatch(ToggleCartHidden())
+})
+
+export default connect(null, mapDispatchToProps)(CartIcon);
