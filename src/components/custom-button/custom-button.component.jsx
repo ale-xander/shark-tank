55622df96935939eefe7fa0c7e6c137f34d2462e
alexiliev <iliev.alex@outlook.com>
2019-10-13 15:46:01 -0700

    addint Add To Cart button to items in collection component

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import './custom-button.styles.scss'
 
-const CustomButton = ({children, isGoogleSignIn,...otherProps}) => (
-    <button className={`${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`}
+const CustomButton = ({inverted, children, isGoogleSignIn,...otherProps}) => (
+    
+    <button className={`
+    ${inverted ? 'inverted' : ''} 
+    ${isGoogleSignIn ? 'google-sign-in' : ''} 
+    custom-button`}
     {...otherProps}>
+
         {children}
+
     </button>
 )
 export default CustomButton;
