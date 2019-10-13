1bfbb255480e12ee7a5f6fed13780ba8827eafd8
alexiliev <iliev.alex@outlook.com>
2019-10-13 16:42:04 -0700

    configuring Redux  actions to make add to cart button saving items to cart reducer

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import CustomButton from '../custom-button/custom-button.component';
+import { AddItem } from '../../redux/cart/cart.actions';
 import './collection-item.styles.scss';
 
-const CollectionItem = ({id, name, price, imageUrl}) => (
+const CollectionItem = ({item, AddItem}) => {
+    const {name, price, imageUrl} = item;
+
+    return (
     <div className="collection-item">
         <div 
         className="image"
@@ -14,7 +19,17 @@ const CollectionItem = ({id, name, price, imageUrl}) => (
                 <span className="name">{name}</span>
                 <span className="price">{price}</span>
             </div>
-        <CustomButton inverted> Add to cart </CustomButton>
+        <CustomButton 
+            onClick={() => AddItem(item)}
+            inverted> 
+        Add to cart 
+        </CustomButton>
     </div>
-)
-export default CollectionItem;
+)};
+
+const mapDispatchToProps = dispatch => ({
+    AddItem: item => dispatch(AddItem(item))
+});
+
+
+export default connect(null,mapDispatchToProps)(CollectionItem);
