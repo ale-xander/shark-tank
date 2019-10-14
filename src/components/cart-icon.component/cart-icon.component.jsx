a4d85209e7404587402a0aeec22866462fb3d1c4
alexiliev <iliev.alex@outlook.com>
2019-10-13 17:48:52 -0700

    adding functionality to display the numbr of tiems in shopping cart

--- src/components/cart-icon.component/cart-icon.component.jsx
+++ src/components/cart-icon.component/cart-icon.component.jsx
@@ -4,15 +4,22 @@ import { ToggleCartHidden } from '../../redux/cart/cart.actions';
 import { ReactComponent as ShoppingIcon } from '../../assets/iconmonstr-shopping-bag-4.svg';
 import './cart-icon.styles.scss';
 
-const CartIcon = ({ToggleCartHidden}) => (
+const CartIcon = ( {ToggleCartHidden, itemCount} ) => (
     <div className="cart-icon" onClick={ToggleCartHidden}>
         <ShoppingIcon className='shopping-icon'/>
-        <span className='item-count'>0</span>
+        <span className='item-count'>{itemCount}</span>
     </div>
 );
 
 const mapDispatchToProps = dispatch => ({
     ToggleCartHidden: ()=> dispatch(ToggleCartHidden())
-})
+});
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+const mapStateToProps = ({ cart:{cartItems} }) => ({
+    itemCount: cartItems.reduce(
+        (accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity, 
+        0
+    )
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
